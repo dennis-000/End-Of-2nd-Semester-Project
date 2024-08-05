2fa936413f3cc4e4ef75e36fcd451d6464ff0870
Dennis Asiedu <odennisasiedu@gmail.com>
2024-08-05 10:53:15 +0000

    first commit

---
+++ assets/js/loader.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+
+    // Hide the loader
+    loader.style.display = 'none';
+
+    // Show the content
+    content.style.display = 'block';
+});
